77617f153d8016d8d0db0c4ff1d26f634368aabc
DAZ <daz4126@gmail.com>
2024-03-21 22:49:44 +0000

    Update koala.js

--- koala.js
+++ koala.js
@@ -3,22 +3,25 @@ import { render } from "preact";
 import { html } from "htm/preact";
 
 // main Koala function
-const Koala = () => {
+const Koala = (data={}) => {
     // secret variable that no one has access to. Access is controlled through methods
-    const settings = {};
+    const settings = {
+        element: document.body,
+        data,
+    };
 
     // return object that gets assigned to the app variable
     return {
-        // used to set the initial data object, stored in the settings object
-        data:  obj => settings.data = obj,
         // view template is stored in the settings object
         template: func => settings.html = func,
         update: func => {
-            settings.data = {...settings.data, ...func(settings.data)}
+            settings.data = {...structuredClone(settings.data), ...func(settings.data)}
             render(settings.html(settings.data), document.body)
         },
+        // set the root element to render to
+        root: el => settings.element = el,
         // renders the template to the page, used for the initial page render
-        render: () => render(settings.html(settings.data), document.body)
+        render: () => render(settings.html(settings.data), settings.element)
     };
   };
   
